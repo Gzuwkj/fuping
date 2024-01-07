8254486c06dc139bc95d2ba9202fe190808b721e
wangjitoan <1729629481@qq.com>
2024-01-07 20:30:26 +0800

    wjt

--- rules/5_17_012.py
+++ rules/5_17_012.py
@@ -3,13 +3,15 @@ from error import Error
 
 def process(record: Person):
     # 5_17_012-未消除风险防止防贫监测对象识别以来家中无县外务工人员但措施登记了外出务工补贴
-    if record.outInfo and record.objectInfo is None:
+    if (record.outInfo and record.objectInfo) is None:
         return
     famliy_num = len(record.family.member)
+    if record.objectInfo['县']==None:
+        print(1111)
     my_city = record.objectInfo['县']
     T = 1
     for i in range(famliy_num):
-        if record.family.member[i].outInfo is None:
+        if record.family.member[i].outInfo is not None:
             last = len(record.family.member[i].outInfo) - 1
             if record.family.member[i].outInfo[last]['务工所在县'] == my_city:
                 T = 0
--- rules/5_17_013.py
+++ rules/5_17_013.py
@@ -3,8 +3,7 @@ from error import Error
 
 def process(record: Person):
     # 5_17_013 - 防止防贫监测对象识别后家中无新增县外务工人员但措施登记了劳务输出
-    if record.outInfo and record.objectInfo is None:
+    if (record.outInfo and record.objectInfo) is None:
         return
-    last = len(record.outInfo) - 1
     if record.objectInfo['监测对象类别'] != '' and record.objectInfo['就业帮扶'] == '劳务输出':
-        raise Error(no='5_17_013', outInfo=record.outInfo,object=[record.objectInfo])
+        raise Error(no='5_17_013', outInfo=record.outInfo,objectInfo=[record.objectInfo])
