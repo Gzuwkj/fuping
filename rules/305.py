1a9c8766fa2af69eaa99d784b152a18d99f1b19f
Administrator <1226992464@qq.com>
2024-01-07 20:01:16 +0800

    rzy

--- rules/305.py
+++ rules/305.py
@@ -12,6 +12,6 @@ def process(record: Person):
         return
     if record.objectInfo['劳动技能']:
         if record.outInfo is None:
-            raise Error(no=os.path.basename(__file__), objectInfo=[record.objectInfo],
+            raise Error(no=os.path.basename(__file__), objectInfo=[record.objectInfo], outInfo=record.outInfo,
                         msg='脱贫人口劳动能力为技能劳动力但未务工就业 ')
 
