1775539504c7a3fe00d6d2dd67848e92d115cf81
tony <1074398416@QQ.com>
2024-01-07 19:57:18 +0800

    Add files via upload

---
+++ rules/2_01_010-16.py
@@ -0,0 +1,25 @@
+from model import Person
+from error import Error
+from typing import Dict, List
+from datetime import datetime
+
+
+def get_age(birthdate):
+    birth_date = datetime.strptime(birthdate, '%Y%m%d')
+    current_date = datetime.now()
+    age = (current_date - birth_date).days // 365
+    return age
+
+
+def process(record: Person):
+    if record.objectInfo is None:
+        return
+    if record.objectInfo["户类型"] == "脱贫户":
+        birthdate = record.objectInfo['出生日期']
+        age = get_age(birthdate)
+        if age >= 16 and age < 60:
+            if record.objectInfo["健康状况"] == "健康":
+                if len(record.objectInfo["在校生状况"]) == 0:
+                    if record.objectInfo["户类型"] == "脱贫户":
+                        if record.objectInfo["劳动技能"] == "无劳动力":
+                            raise Error(no='2_01_010-16', objectInfo=[record.objectInfo])
