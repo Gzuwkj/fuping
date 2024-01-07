b63d2a81dbe688cddcc23898de6c3e8a6b44f9fc
lulu3339 <@lulu3339>
2024-01-07 20:18:51 +0800

    xiugai2

--- rules/5_16_002.py
+++ rules/5_16_002.py
@@ -3,6 +3,7 @@ import pandas as pd
 from error import Error
 '''5_16_002-目前务工脱贫人口填写的务工月收入收入大于30000元或者小于100元'''
 
+result = []
 def process(record: Person):
     if record.outInfo is None or record.objectInfo is None:
         return
@@ -15,10 +16,9 @@ def process(record: Person):
                 yueshouru = float(yueshouru)
             except:
                 raise Error(no='5_16_002', outInfo=record.outInfo)
-                break
 
         if i.get("户类型") == "脱贫户" and (yueshouru > 30000 or yueshouru < 100):
-            raise Error(no='5_16_002', outInfo=record.outInfo)
-            break
+            result.append(i)
+    raise Error(no='5_16_002', outInfo=result)
 
 
