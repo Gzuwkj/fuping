5d4ec3ddcdd45e9874b9cf4baac7e85b01789de1
dawnabout <1582177310@qq.com>
2024-01-10 16:08:17 +0800

    wucan

--- rules/3_01_009.py
+++ rules/3_01_009.py
@@ -8,11 +8,13 @@ from datetime import datetime
 
 id2record: Dict[str, List[Person]] = {}
 
-
+result = []
 def process(record: Person):
     if record.outInfo is not None:
-        out_info = record.outInfo
-        is_working = out_info.get("是否已务工")
-        if is_working is None:
-            raise Error(no='3_01_009', outInfo=[out_info])
+        for index, out_info in enumerate(record.outInfo, start=1):
+            is_working = out_info.get("是否已务工")
+            if is_working is None:
+                result.append(out_info)
+        if result:
+            raise Error(no='3_01_009', outInfo=result)
 
