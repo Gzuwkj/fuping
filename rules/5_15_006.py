94db7b4694a10b82138115887c216c509678c318
yujing <18085192677@163.com>
2024-01-16 20:48:33 +0800

    yujing

--- rules/5_15_006.py
+++ rules/5_15_006.py
@@ -7,11 +7,12 @@ def process(record: Person):
     if record.objectInfo is None:
         return
     for m in record.family.member:
-        public_welfare = str(m.objectInfo.get('公益性岗位'))
-        if public_welfare != '':
-            labour_skill = str(m.objectInfo.get('劳动技能'))
-            if labour_skill == '无劳动能力' or labour_skill == '丧失劳动力':
-                raise Error(no='5_15_006', objectInfo=[record.objectInfo])
+        if m.objectInfo:
+            public_welfare = str(m.objectInfo.get('公益性岗位'))
+            if public_welfare != '':
+                labour_skill = str(m.objectInfo.get('劳动技能'))
+                if labour_skill == '无劳动能力' or labour_skill == '丧失劳动力':
+                    raise Error(no='5_15_006', objectInfo=[record.objectInfo])
 
 
 
